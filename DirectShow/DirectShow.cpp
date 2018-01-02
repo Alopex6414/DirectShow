e9aa7d2629f604a104bbc61ac19f42e52f7fa153
Helium <1029535012@qq.com>
2018-01-02 14:04:36 +0800

    DirectShow v1.11a

--- DirectShow/DirectShow.cpp
+++ DirectShow/DirectShow.cpp
@@ -6,9 +6,10 @@
 * @file		DirectShow.cpp
 * @brief	This Program is DirectShow DLL Project.
 * @author	Alopex/Helium
-* @version	v1.10a
-* @date		2017-11-1	v1.00a	alopex	Create This Project
-* @date		2017-12-8	v1.10a	alopex	Code Do Not Rely On MSVCR Library
+* @version	v1.11a
+* @date		2017-11-1	v1.00a	alopex	Create This Project.
+* @date		2017-12-8	v1.10a	alopex	Code Do Not Rely On MSVCR Library.
+* @date		2018-1-2	v1.11a	alopex	Make Demo And Add Video Play In Window Mode.
 */
 #include "DirectCommon.h"
 #include "DirectShow.h"
@@ -30,6 +31,10 @@ DirectShow::DirectShow()
 	m_pDirectShowBasicAudio = NULL;		//IBasicAudio接口对象指针初始化(NULL)
 	m_pDirectShowBasicVideo = NULL;		//IBasicVideo接口对象指针初始化(NULL)
 	m_pDirectShowVideoWindow = NULL;	//IVideoWindow接口对象指针初始化(NULL)
+
+	m_lVideoWidth = 0;					//Video Origin Width(视频源宽度)
+	m_lVideoHeight = 0;					//Video Origin Height(视频源高度)
+	m_fVideofps = 0.0f;					//Video Origin Frame Per Second(fps)(~视频源场频)
 }
 
 //------------------------------------------------------------------
@@ -49,6 +54,42 @@ DirectShow::~DirectShow()
 	SAFE_RELEASE(m_pDirectShowMediaControl);	//IMediaControl释放
 	SAFE_RELEASE(m_pDirectShowGraphBuilder);	//IGraphBuilder释放
 	CoUninitialize();							//COM释放
+}
+
+//------------------------------------------------------------------
+// @Function:	 DirectShowGetVideoWidth(void) const
+// @Purpose: DirectShow 获取视频宽度
+// @Since: v1.00a
+// @Para: None
+// @Return: long
+//------------------------------------------------------------------
+long WINAPI DirectShow::DirectShowGetVideoWidth(void) const
+{
+	return m_lVideoWidth;
+}
+
+//------------------------------------------------------------------
+// @Function:	 DirectShowGetVideoHeight(void) const
+// @Purpose: DirectShow 获取视频高度
+// @Since: v1.00a
+// @Para: None
+// @Return: long
+//------------------------------------------------------------------
+long WINAPI DirectShow::DirectShowGetVideoHeight(void) const
+{
+	return m_lVideoHeight;
+}
+
+//------------------------------------------------------------------
+// @Function:	 DirectShowGetVideoFramePerSecond(void) const
+// @Purpose: DirectShow 获取视频场频
+// @Since: v1.00a
+// @Para: None
+// @Return: long
+//------------------------------------------------------------------
+float WINAPI DirectShow::DirectShowGetVideoFramePerSecond(void) const
+{
+	return m_fVideofps;
 }
 
 //------------------------------------------------------------------
@@ -277,12 +318,39 @@ void WINAPI DirectShow::DirectShowMP3SetCurrentPosition(REFTIME RefPosition)
 
 //------------------------------------------------------------------
 // @Function:	 DirectShowGetVideoInfo(void)
-// @Purpose: DirectShow 读取视频文件信息
+// @Purpose: DirectShow 获取视频格式信息
 // @Since: v1.00a
 // @Para: HWND hWnd(窗口句柄)
 // @Return: HRESULT(初始化状态:成功:S_OK,失败:E_FAIL)
 //------------------------------------------------------------------
-HRESULT WINAPI DirectShow::DirectShowGetVideoInfo(HWND hWnd)
+HRESULT WINAPI DirectShow::DirectShowGetVideoInfo(void)
+{
+	long VideoWidth;
+	long VideoHeight;
+	REFTIME AvgTimePerFrame;
+	float VideoFramePerSecond;
+
+	//视频Video
+	VERIFY(m_pDirectShowGraphBuilder->QueryInterface(IID_IBasicVideo, (void **)&m_pDirectShowBasicVideo));//创建IBasicVideo接口
+	VERIFY(m_pDirectShowBasicVideo->get_VideoWidth(&VideoWidth));	//读取视频宽度
+	VERIFY(m_pDirectShowBasicVideo->get_VideoHeight(&VideoHeight));	//读取视频高度
+	VERIFY(m_pDirectShowBasicVideo->get_AvgTimePerFrame(&AvgTimePerFrame));	//读取视频帧率
+	VideoFramePerSecond = (float)(1 / AvgTimePerFrame);	//计算视频场频
+	m_lVideoWidth = VideoWidth;	//视频宽度
+	m_lVideoHeight = VideoHeight;	//视频高度
+	m_fVideofps = VideoFramePerSecond;	//视频场频
+
+	return S_OK;//OK
+}
+
+//------------------------------------------------------------------
+// @Function:	 DirectShowSetVideoPlayInWindow(HWND hWnd)
+// @Purpose: DirectShow 设置视频在指定窗口内播放
+// @Since: v1.00a
+// @Para: HWND hWnd(窗口句柄)
+// @Return: HRESULT(初始化状态:成功:S_OK,失败:E_FAIL)
+//------------------------------------------------------------------
+HRESULT WINAPI DirectShow::DirectShowSetVideoPlayInWindow(HWND hWnd)
 {
 	RECT WindowRect;
 	long VideoWidth;
@@ -296,6 +364,9 @@ HRESULT WINAPI DirectShow::DirectShowGetVideoInfo(HWND hWnd)
 	VERIFY(m_pDirectShowBasicVideo->get_VideoHeight(&VideoHeight));	//读取视频高度
 	VERIFY(m_pDirectShowBasicVideo->get_AvgTimePerFrame(&AvgTimePerFrame));	//读取视频帧率
 	VideoFramePerSecond = (float)(1 / AvgTimePerFrame);	//计算视频场频
+	m_lVideoWidth = VideoWidth;	//视频宽度
+	m_lVideoHeight = VideoHeight;	//视频高度
+	m_fVideofps = VideoFramePerSecond;	//视频场频
 
 	//音频Audio
 	VERIFY(m_pDirectShowGraphBuilder->QueryInterface(IID_IBasicAudio, (void**)&m_pDirectShowBasicAudio));//创建IBasicAudio接口
@@ -309,9 +380,72 @@ HRESULT WINAPI DirectShow::DirectShowGetVideoInfo(HWND hWnd)
 	m_pDirectShowVideoWindow->put_Top(0);	//视频原点Y坐标
 	m_pDirectShowVideoWindow->put_Width(WindowRect.right - WindowRect.left);	//视频宽度
 	m_pDirectShowVideoWindow->put_Height(WindowRect.bottom - WindowRect.top);	//视频高度
-	m_pDirectShowVideoWindow->put_WindowStyle(WS_CHILD | WS_CLIPCHILDREN | WS_CLIPSIBLINGS | WS_THICKFRAME);	//窗口模式
+	m_pDirectShowVideoWindow->put_WindowStyle(WS_CHILD | WS_CLIPCHILDREN | WS_CLIPSIBLINGS);	//窗口模式
 	m_pDirectShowVideoWindow->put_MessageDrain((OAHWND)hWnd);//设置接受窗口信息句柄  
 	m_pDirectShowVideoWindow->put_Visible(OATRUE);
+
+	return S_OK;//OK
+}
+
+//------------------------------------------------------------------
+// @Function:	 DirectShowSetVideoPlayInWindow(HWND hWnd, RECT sRect)
+// @Purpose: DirectShow 设置视频在指定窗口内播放
+// @Since: v1.00a
+// @Para: HWND hWnd(窗口句柄)
+// @Para: RECT sRect(视频播放区域)
+// @Return: HRESULT(初始化状态:成功:S_OK,失败:E_FAIL)
+//------------------------------------------------------------------
+HRESULT WINAPI DirectShow::DirectShowSetVideoPlayInWindow(HWND hWnd, RECT sRect)
+{
+	long VideoWidth;
+	long VideoHeight;
+	REFTIME AvgTimePerFrame;
+	float VideoFramePerSecond;
+
+	//视频Video
+	VERIFY(m_pDirectShowGraphBuilder->QueryInterface(IID_IBasicVideo, (void **)&m_pDirectShowBasicVideo));//创建IBasicVideo接口
+	VERIFY(m_pDirectShowBasicVideo->get_VideoWidth(&VideoWidth));	//读取视频宽度
+	VERIFY(m_pDirectShowBasicVideo->get_VideoHeight(&VideoHeight));	//读取视频高度
+	VERIFY(m_pDirectShowBasicVideo->get_AvgTimePerFrame(&AvgTimePerFrame));	//读取视频帧率
+	VideoFramePerSecond = (float)(1 / AvgTimePerFrame);	//计算视频场频
+	m_lVideoWidth = VideoWidth;	//视频宽度
+	m_lVideoHeight = VideoHeight;	//视频高度
+	m_fVideofps = VideoFramePerSecond;	//视频场频
+
+	//音频Audio
+	VERIFY(m_pDirectShowGraphBuilder->QueryInterface(IID_IBasicAudio, (void**)&m_pDirectShowBasicAudio));//创建IBasicAudio接口
+
+	//窗口Window
+	VERIFY(m_pDirectShowGraphBuilder->QueryInterface(IID_IVideoWindow, (void**)&m_pDirectShowVideoWindow));//创建IVideoWindow接口
+	m_pDirectShowVideoWindow->put_Visible(OAFALSE);
+	m_pDirectShowVideoWindow->put_Owner((OAHWND)hWnd);	//设置视频窗口句柄
+	m_pDirectShowVideoWindow->put_Left(sRect.left);	//视频原点X坐标
+	m_pDirectShowVideoWindow->put_Top(sRect.top);	//视频原点Y坐标
+	m_pDirectShowVideoWindow->put_Width(sRect.right - sRect.left);	//视频宽度
+	m_pDirectShowVideoWindow->put_Height(sRect.bottom - sRect.top);	//视频高度
+	m_pDirectShowVideoWindow->put_WindowStyle(WS_CHILD | WS_CLIPCHILDREN | WS_CLIPSIBLINGS);	//窗口模式
+	m_pDirectShowVideoWindow->put_MessageDrain((OAHWND)hWnd);//设置接受窗口信息句柄  
+	m_pDirectShowVideoWindow->put_Visible(OATRUE);
+
+	return S_OK;//OK
+}
+
+//------------------------------------------------------------------
+// @Function:	 DirectShowVideoPlayWait(void)
+// @Purpose: DirectShow 播放视频文件(等待)
+// @Since: v1.00a
+// @Para: None
+// @Return: HRESULT(初始化状态:成功:S_OK,失败:E_FAIL)
+//------------------------------------------------------------------
+HRESULT WINAPI DirectShow::DirectShowVideoPlayWait(void)
+{
+	long evCode = 0;
+
+	VERIFY(m_pDirectShowMediaControl->Run());
+	while (evCode != EC_COMPLETE)
+	{
+		m_pDirectShowMediaEvent->WaitForCompletion(1000, &evCode);
+	}
 
 	return S_OK;//OK
 }
@@ -325,13 +459,7 @@ HRESULT WINAPI DirectShow::DirectShowGetVideoInfo(HWND hWnd)
 //------------------------------------------------------------------
 HRESULT WINAPI DirectShow::DirectShowVideoPlay(void)
 {
-	long evCode = 0;
-
 	VERIFY(m_pDirectShowMediaControl->Run());
-	while (evCode != EC_COMPLETE)
-	{
-		m_pDirectShowMediaEvent->WaitForCompletion(1000, &evCode);
-	}
 
 	return S_OK;//OK
 }
--- DirectShow/DirectShow.h
+++ DirectShow/DirectShow.h
@@ -6,9 +6,10 @@
 * @file		DirectShow.h
 * @brief	This Program is DirectShow DLL Project.
 * @author	Alopex/Helium
-* @version	v1.10a
-* @date		2017-11-1	v1.00a	alopex	Create This Project
-* @date		2017-12-8	v1.10a	alopex	Code Do Not Rely On MSVCR Library
+* @version	v1.11a
+* @date		2017-11-1	v1.00a	alopex	Create This Project.
+* @date		2017-12-8	v1.10a	alopex	Code Do Not Rely On MSVCR Library.
+* @date		2018-1-2	v1.11a	alopex	Make Demo And Add Video Play In Window Mode.
 */
 #pragma once
 
@@ -43,37 +44,55 @@ private:
 	IBasicVideo* m_pDirectShowBasicVideo;		//IBasicVideo Interface Pointer
 	IVideoWindow* m_pDirectShowVideoWindow;		//IVideoWindow Interface Pointer
 
+	long m_lVideoWidth;							//Video Origin Width
+	long m_lVideoHeight;						//Video Origin Height
+	float m_fVideofps;							//Video Origin Frame Per Second(fps)
+
 public:
-	DirectShow();	//DirectShow Constructor Function
-	~DirectShow();	//DirectShow Destructor Function
+	DirectShow();	//DirectShow Constructor Function(DirectShow构造函数)
+	~DirectShow();	//DirectShow Destructor Function(DirectShow析构函数)
 
-	HRESULT WINAPI DirectShowInit(void);	//DirectShow Initialize
-	HRESULT WINAPI DirectShowLoadFile(LPWSTR lpszFileName);	//DirectShow Load Audio/Video File
-	HRESULT WINAPI DirectShowLoadMP3(LPWSTR lpszFileName);	//DirectShow Load MP3 File
-	HRESULT WINAPI DirectShowLoadAudio(LPWSTR lpszFileName);	//DirectShow Load Audio File
-	HRESULT WINAPI DirectShowLoadVideo(LPWSTR lpszFileName);	//DirectShow Load Video File
+	//DirectShow访问
+	long WINAPI DirectShowGetVideoWidth(void) const;	//DirectShow Get Video Width(获取视频宽度)
+	long WINAPI DirectShowGetVideoHeight(void) const;	//DirectShow Get Video Height(获取视频高度)
+	float WINAPI DirectShowGetVideoFramePerSecond(void) const;	//DirectShow Get Video fps(获取视频场频)
 
-	void WINAPI DirectShowMP3Play(void);	//DirectShow MP3 Play
-	void WINAPI DirectShowMP3Pause(void);	//DirectShow MP3 Pause
-	void WINAPI DirectShowMP3Stop(void);	//DirectShow MP3 Stop
-	void WINAPI DirectShowMP3GetDuration(REFTIME* pRefDuration) const;	//DirectShow MP3 GetDuration
-	void WINAPI DirectShowMP3GetCurrentPosition(REFTIME* pRefPosition) const;	//DirectShow MP3 GetCurrentPosition
-	void WINAPI DirectShowMP3SetCurrentPosition(REFTIME RefPosition);	//DirectShow MP3 SetCurrentPosition
+	//DirectShow初始化
+	HRESULT WINAPI DirectShowInit(void);	//DirectShow Initialize(DirectShow初始化)
 
-	void WINAPI DirectShowAudioPlay(void);	//DirectShow Audio Play
-	void WINAPI DirectShowAudioPause(void);	//DirectShow Audio Pause
-	void WINAPI DirectShowAudioStop(void);	//DirectShow Audio Stop
-	void WINAPI DirectShowAudioGetDuration(REFTIME* pRefDuration) const;//DirectShow Audio GetDuration
-	void WINAPI DirectShowAudioGetCurrentPosition(REFTIME* pRefPosition) const;	//DirectShow Audio GetCurrentPosition
-	void WINAPI DirectShowAudioSetCurrentPosition(REFTIME RefPosition);	//DirectShow Audio SetCurrentPosition
+	//DirectShow加载文件(路径)
+	HRESULT WINAPI DirectShowLoadFile(LPWSTR lpszFileName);	//DirectShow Load Audio/Video File(DirectShow加载文件)
+	HRESULT WINAPI DirectShowLoadMP3(LPWSTR lpszFileName);	//DirectShow Load MP3 File(DirectShow加载MP3文件)
+	HRESULT WINAPI DirectShowLoadAudio(LPWSTR lpszFileName);	//DirectShow Load Audio File(DirectShow加载音频文件)
+	HRESULT WINAPI DirectShowLoadVideo(LPWSTR lpszFileName);	//DirectShow Load Video File(DirectShow加载视频文件)
 
-	HRESULT WINAPI DirectShowGetVideoInfo(HWND hWnd);	//DirectShow Get Video Information
-	HRESULT WINAPI DirectShowVideoPlay(void);	//DirectShow Video Play
-	HRESULT WINAPI DirectShowVideoPause(void);	//DirectShow Video Pause
-	HRESULT WINAPI DirectShowVideoStop(void);	//DirectShow Video Stop
-	HRESULT WINAPI DirectShowVideoGetDuration(REFTIME* pRefDuration) const;	//DirectShow Video GetDuration
-	HRESULT WINAPI DirectShowVideoGetCurrentPosition(REFTIME* pRefPosition) const;	//DirectShow Video GetCurrentPosition
-	HRESULT WINAPI DirectShowVideoSetCurrentPosition(REFTIME RefPosition);	//DirectShow Video SetCurrentPosition
+	//DirectShow播放MP3文件格式
+	void WINAPI DirectShowMP3Play(void);	//DirectShow MP3 Play(DirectShow播放)
+	void WINAPI DirectShowMP3Pause(void);	//DirectShow MP3 Pause(DirectShow暂停)
+	void WINAPI DirectShowMP3Stop(void);	//DirectShow MP3 Stop(DirectShow停止)
+	void WINAPI DirectShowMP3GetDuration(REFTIME* pRefDuration) const;	//DirectShow MP3 GetDuration(DirectShow获取时间总长度)
+	void WINAPI DirectShowMP3GetCurrentPosition(REFTIME* pRefPosition) const;	//DirectShow MP3 GetCurrentPosition(DirectShow获取当前进度)
+	void WINAPI DirectShowMP3SetCurrentPosition(REFTIME RefPosition);	//DirectShow MP3 SetCurrentPosition(DirectShow设置当前进度)
+
+	//DirectShow播放音频文件格式
+	void WINAPI DirectShowAudioPlay(void);	//DirectShow Audio Play(DirectShow播放)
+	void WINAPI DirectShowAudioPause(void);	//DirectShow Audio Pause(DirectShow暂停)
+	void WINAPI DirectShowAudioStop(void);	//DirectShow Audio Stop(DirectShow停止)
+	void WINAPI DirectShowAudioGetDuration(REFTIME* pRefDuration) const;//DirectShow Audio GetDuration(DirectShow获取时间总长度)
+	void WINAPI DirectShowAudioGetCurrentPosition(REFTIME* pRefPosition) const;	//DirectShow Audio GetCurrentPosition(DirectShow获取当前进度)
+	void WINAPI DirectShowAudioSetCurrentPosition(REFTIME RefPosition);	//DirectShow Audio SetCurrentPosition(DirectShow设置当前进度)
+
+	//DirectShow播放视频文件格式
+	HRESULT WINAPI DirectShowGetVideoInfo(void);	//DirectShow Get Video Information(DirectShow获取视频格式)
+	HRESULT WINAPI DirectShowSetVideoPlayInWindow(HWND hWnd);	//DirectShow Set Video Play In Window(DirectShow设置视频在指定窗口内播放)(~窗口)
+	HRESULT WINAPI DirectShowSetVideoPlayInWindow(HWND hWnd, RECT sRect);	//DirectShow Set Video Play In Window(DirectShow设置视频在指定窗口内播放)(~指定矩形区域)
+	HRESULT WINAPI DirectShowVideoPlayWait(void);	//DirectShow Video Play Wait(DirectShow播放)(等待)
+	HRESULT WINAPI DirectShowVideoPlay(void);	//DirectShow Video Play(DirectShow播放)
+	HRESULT WINAPI DirectShowVideoPause(void);	//DirectShow Video Pause(DirectShow暂停)
+	HRESULT WINAPI DirectShowVideoStop(void);	//DirectShow Video Stop(DirectShow停止)
+	HRESULT WINAPI DirectShowVideoGetDuration(REFTIME* pRefDuration) const;	//DirectShow Video GetDuration(DirectShow获取时间总长度)
+	HRESULT WINAPI DirectShowVideoGetCurrentPosition(REFTIME* pRefPosition) const;	//DirectShow Video GetCurrentPosition(DirectShow获取当前进度)
+	HRESULT WINAPI DirectShowVideoSetCurrentPosition(REFTIME RefPosition);	//DirectShow Video SetCurrentPosition(DirectShow设置当前进度)
 };
 
 #endif
